0ae5f1db82eb7b02726d6434e744c6586f2e6429
Athul0491 <65664185+Athul0491@users.noreply.github.com>
2023-11-01 14:47:20 +0530

    fix: syntax issues

--- src/auth/server.py
+++ src/auth/server.py
@@ -5,11 +5,11 @@ from flask_mysqldb import MySQL
 server = Flask(__name__)
 mysql = MySQL(server)
 
-server.config['MYSQL_HOST'] = os.environ.get['MYSQL_HOST']
-server.config['MYSQL_USER'] = os.environ.get['MYSQL_USER']
-server.config['MYSQL_PASSWORD'] = os.environ.get['MYSQL_PASSWORD']
-server.config['MYSQL_DB'] = os.environ.get['MYSQL_DB']
-server.config['MYSQL_PORT'] = os.environ.get['MYSQL_PORT']
+server.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+server.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+server.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+server.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+server.config['MYSQL_PORT'] = os.environ['MYSQL_PORT']
 
 @server.route('/login', methods=['POST'])
 def login():
